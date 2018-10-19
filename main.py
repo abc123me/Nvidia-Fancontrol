91f52b87371aa67e682366ad4c0a772e50afbf8b
Jeremiah Lowe <jeremiah_abc123me@hotmail.com>
2018-10-19 19:12:41 -0400

    Minor improvements

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ if(__name__ == '__main__'):
 			quit(1)
 	except FileNotFoundError:
 		print("\x1b[01;31m;NVIDIA drivers are not installed, this tool only works with the proprietary NVIDIA drivers\x1b[0m")
+		quit(1)
 	print("\x1b[01;32mFan control works\x1b[0m")
 	lastTemp = getGpuTemp() + 1
 	def shutdownHook():
@@ -47,23 +48,27 @@ if(__name__ == '__main__'):
 		else:
 			print("\x1b[01;32mDisabled fan control\x1b[0m")
 	atexit.register(shutdownHook)
-	while True:
-		temp = getGpuTemp()
-		if(temp != lastTemp):
-			speed = interpFanCurve(defaultFanCurve, temp)
-			change = temp - lastTemp
-			col = "\x1b[01;31m"
-			if(change < 0):
-				col = "\x1b[01;34m"
-			print(col + "Temperature changed by " + str(change) + " deg. C\x1b[0m")
-			estNextTemp = temp
-			if(change > 0):
-				estNextTemp = temp + change * changeFactor
-			print("\x1b[01;33mFan speed thus changed to " + str(speed) + "% ("+ str(temp) + " deg C, " + str(estNextTemp) + " estimated next)\x1b[0m")
-			res = trySetFanSpeed(estNextTemp, legacy=legacyFanSpeed)
-			if(not res[0]):
-				print("\x1b[01;31mFailed to set fan speed, exiting!\x1b[0m")
-				print("\x1b[01;31mReason: %s != %s\x1b[0m" % (res[1], res[2]));
-				quit(-1)
-			lastTemp = temp
-		sleep(1)
+	try:
+		while True:
+			temp = getGpuTemp()
+			if(temp != lastTemp):
+				speed = interpFanCurve(defaultFanCurve, temp)
+				change = temp - lastTemp
+				col = "\x1b[01;31m"
+				if(change < 0):
+					col = "\x1b[01;34m"
+				print(col + "Temperature changed by " + str(change) + " deg. C\x1b[0m")
+				estNextTemp = temp
+				if(change > 0):
+					estNextTemp = temp + change * changeFactor
+				print("\x1b[01;33mFan speed thus changed to " + str(speed) + "% ("+ str(temp) + " deg C, " + str(estNextTemp) + " estimated next)\x1b[0m")
+				res = trySetFanSpeed(estNextTemp, legacy=legacyFanSpeed)
+				if(not res[0]):
+					print("\x1b[01;31mFailed to set fan speed, exiting!\x1b[0m")
+					print("\x1b[01;31mReason: %s != %s\x1b[0m" % (res[1], res[2]));
+					quit(-1)
+				lastTemp = temp
+			sleep(1)
+	except KeyboardInterrupt:
+		print("\x1b[01;31mExiting now!\x1b[0m")
+		quit(1)

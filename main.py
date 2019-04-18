53d0309033c0f812b343bc8b85e718ce3fbcf6c9
Jeremiah Lowe <8075565+abc123me@users.noreply.github.com>
2019-04-17 20:08:36 -0400

    Added kostages changes, added emulation, min speed

--- main.py
+++ main.py
@@ -2,84 +2,112 @@
 
 from nvfan import *
 import atexit
-from math import sqrt
 
-#Some default stuff so you can use this script out-of-the-box
-defaultFanCurve = [
-	(40, 30),
-	(50, 45),
-	(60, 55),
-	(70, 65),
-	(80, 75)
+# 2 curves to achieve hysteresis
+# curve to be used when speed = 0
+highFanCurve = [
+	(20, 0),
+	(30, 0),
+	(35, 10),
+	(40, 20),
+	(45, 30),
+	(50, 40),
+	(60, 50),
+	(70, 70),
+	(80, 90),
+	(90, 100)
+]
+# curve to be used when speed > 0
+lowFanCurve = [
+	(20, 0),
+	(30, 0),
+	(35, 0),
+	(40, 10),
+	(45, 20),
+	(55, 30),
+	(60, 30),
+	(70, 50),
+	(80, 70)
 ]
 
-lastTemp = -1
-changeFactor = 1
+changeFactor = 2.5
+changeThreshold = 1 # The amount of change (in celcius) required to loop
+
+def help():
+	print("-----------HELP-----------")
+	print("-h:         This help menu")
+	print("-e:         Emulate fan control")
+	print("-v:         Verbose mode")
+	print("-u <float>: Set update rate")
+	print("-----------HELP-----------")
+	quit(0)
 
 if(__name__ == '__main__'):
 	import sys
-	if "-v" in sys.argv:
-		setVerboseExecution(True)
-		print("\x1b[01;33mVerbose execution enabled!\x1b[0m")
+	if "-h" in sys.argv: help()
+	if "-e" in sys.argv: setEmulation(True, 420.0)
+	if "-v" in sys.argv: setVerboseExecution(True)
+	speed = 0.0
+	temp = 0.0
 	try:
-		print("\x1b[01;34mTesting fan control\x1b[0m")
+		print_color(BLUE, "Testing fan control")
 		res = trySetFanControlEnabled(True)
 		if(not res[0]):
-			print("\x1b[01;31mUnable to enable fan control!\x1b[0m")
-			print("\x1b[01;31mReason: %s != %s %s\x1b[0m" % (res[1], res[2], res[3]));
+			print_color(RED, "Unable to enable fan control!")
+			print_color(RED, "Reason: %s != %s %s % (res[1], res[2], res[3])");
 			quit(1)
+		print_color(YELLOW, "Checking legacy")
 		legacyFanSpeed = shouldUseLegacyFanSpeed()
 		if(legacyFanSpeed):
-			print("\x1b[01;33mUsing legacy fan speed!\x1b[0m")
-		res = trySetFanSpeed(30, legacy=legacyFanSpeed)
+			print_color(YELLOW, "Using legacy fan speed!")
+		res = trySetFanSpeed(50, legacy=legacyFanSpeed)
 		if(not res[0]):
-			print("\x1b[01;31mUnable to set fan speed!\x1b[0m")
-			print("\x1b[01;31mReason: %s != %s\x1b[0m" % (res[1], res[2]));
+			print_color(RED, "Unable to set fan speed!")
+			print_color(RED, "Reason: %s != %s % (res[1], res[2])");
 			trySetFanControlEnabled(False)
 			quit(1)
 	except FileNotFoundError:
-		print("\x1b[01;31m;NVIDIA drivers are not installed, this tool only works with the proprietary NVIDIA drivers\x1b[0m")
+		print_color(RED, "NVIDIA drivers are not installed, this tool only works with the proprietary NVIDIA drivers")
 		quit(1)
-	print("\x1b[01;32mFan control works\x1b[0m")
+	print_color(GREEN, "Fan control works")
+	
 	lastTemp = getGpuTemp() + 1
+
 	def shutdownHook():
-		print("\x1b[01;33mShutting down!\x1b[0m")
+		print_color(YELLOW, "Shutting down!")
 		res = trySetFanControlEnabled(False)
 		if(not res[0]):
-			print("\x1b[01;31mUnable to disable fan control\x1b[0m")
-			print("\x1b[01;31mReason: %s != %s\x1b[0m" % (res[1], res[2]));
+			print_color(RED, "Unable to disable fan control")
+			print_color(RED, "Reason: %s != %s % (res[1], res[2])");
 		else:
-			print("\x1b[01;32mDisabled fan control\x1b[0m")
+			print_color(GREEN, "Disabled fan control")
 	atexit.register(shutdownHook)
 	try:
 		while True:
 			temp = getGpuTemp()
 			if(temp != lastTemp):
 				change = temp - lastTemp
-				col = "\x1b[01;31m"
+				col = RED
 				if(change < 0):
-					col = "\x1b[01;34m"
-					estNextTemp = temp
-					speed = interpFanCurve(defaultFanCurve, estNextTemp, incMode=False)
-				print(col + "Temperature changed by " + str(change) + " deg. C\x1b[0m")
+					col = BLUE
+				print_color(col, "Temperature changed by " + str(change) + " deg. C")
+				estNextTemp = temp
 				if(change > 0):
-					estNextTemp = temp + sqrt(change) * changeFactor
-					speed = interpFanCurve(defaultFanCurve, estNextTemp, incMode=True)
-				print("\x1b[01;33mFan speed thus changed to " + str(speed) + "% ("+ str(temp) + " deg C, " + str(estNextTemp) + " estimated next)\x1b[0m")
-				if(speed != -1):
-					res = trySetFanSpeed(speed, fan=0, legacy=legacyFanSpeed)
-					if(not res[0]):
-						print("\x1b[01;31mFailed to set fan speed, exiting!\x1b[0m")
-						print("\x1b[01;31mReason: %s != %s\x1b[0m" % (res[1], res[2]));
-						quit(-1)
-
-					res = trySetFanSpeed(speed, fan=1, legacy=legacyFanSpeed)
-					if(not res[0]):
-						print("\x1b[01;31mFailed to set fan speed, exiting!\x1b[0m")
-						print("\x1b[01;31mReason: %s != %s\x1b[0m" % (res[1], res[2]));
-						quit(-1)
+					estNextTemp = temp + change * changeFactor
+				# add hysteresis
+				if (speed > 0):
+					funCurve = lowFanCurve
+				else:
+					funCurve = highFanCurve
+				speed = interpFanCurve(funCurve, estNextTemp)
+				print_color(YELLOW, "Fan speed thus changed to " + str(speed) + "% (" + str(temp) + " deg C, " + str(estNextTemp) + " estimated next)")
+				res = trySetFanSpeed(speed, legacy=legacyFanSpeed)
+				if(not res[0]):
+					print_color(RED, "Failed to set fan speed, exiting!")
+					print_color(RED, "Reason: %s != %s % (res[1], res[2])");
+					quit(-1)
 				lastTemp = temp
-			sleep(4)
+				sleep(1)
 	except KeyboardInterrupt:
-		print("\x1b[01;31mExiting now!\x1b[0m")
+		print_color(RED, "Exiting now!")
 		quit(1)
--- nvfan.py
+++ nvfan.py
@@ -1,19 +1,56 @@
 from time import sleep
 from socket import gethostname
 import subprocess, os
+import sys
 
 _hostName = gethostname()
 _verboseExecution = False
+_emulating = False
+_emulatingVersion = 420.0
+_minRevSpeed = 20
+_ansiEnabled = True
 
+NC = 0
+YELLOW = 1
+RED = 2
+GREEN = 3
+BLUE = 4
+
+color_pref = [
+	"\x1b[0m",
+	"\x1b[01;33m",
+	"\x1b[01;31m",
+	"\x1b[01;32m",
+	"\x1b[01;34m"
+]
+
+#Sets the minimum speed
+def setMinimumSpeed(m):
+	global _minRevSpeed
+	if _verboseExecution: print_color(YELLOW, "Minimum speed set to " + m)
+	_minRevSpeed = m
+def getMimimumSpeed():
+	return _minRevSpeed
+#Turns off/on emulation
+def setEmulation(e, version=420.0):
+	global _emulating,_emulatingVersion
+	if e: print_color(YELLOW, "Emulation enabled")
+	else: print_color(YELLOW, "Emulation disabled")
+	_emulating = e
+	_emulatingVersion = version
+#Prints colored text
+def print_color(color, *args, **kwargs):
+	if _ansiEnabled: print(color_pref[color] + "".join(map(str,args)) + "\x1b[0m", **kwargs)
+	else: print("".join(map(str,args)) + "\x1b[0m", **kwargs)
 #Enables / Disables verbose execution
 def setVerboseExecution(en):
 	global _verboseExecution
+	if en: print_color(YELLOW, "Verbose execution enabled!")
 	_verboseExecution = en
 #Executes a command and returns a tuple of the following, (exit code, stdout string, stderr string)
 def execCmd(cmd, encoding='utf-8'):
-	global _verboseExecution
-	if _verboseExecution:
-		print("execCmd(cmd): %s" % (str(cmd)))
+	if _emulating: return None
+	if _verboseExecution: print_color(NC, "execCmd(cmd): %s" % (str(cmd)))
 	p = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	return (int(p.returncode), str(p.stdout.decode(encoding)), str(p.stderr.decode(encoding)))
 #Gets the current XDisplay the process is using
@@ -21,100 +58,73 @@ def getXDisplay():
 	return os.environ['DISPLAY'].split(".")[0]
 #Gets the GPU's temperature using 'nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader'
 def getGpuTemp(gpu=0):
+	if _emulating:
+		from math import sin
+		from time import time
+		return (30 + 30 * ((1 + sin(time() / 10000)) / 2))
 	res = execCmd(['nvidia-smi', '-i', str(gpu), '--query-gpu=temperature.gpu', '--format=csv,noheader'])
-	if(res[0] == 0):
-		return int(res[1])
+	if res[0] == 0: return int(res[1])
 	else:
-		print('Could not get GPU temperature!')
-		print('Output: ' + str(res[1]))
+		print_color(NC, 'Could not get GPU temperature!')
+		print_color(NC, 'Output: ' + str(res[1]))
 		quit()
 #Returns the current driver version
 def getDriverVersion(gpu=0):
+	if _emulating: return _emulatingVersion
 	res = execCmd(['nvidia-smi', '-i', str(gpu), '--query-gpu=driver_version', '--format=csv,noheader'])
 	versionSplit = res[1].split('.')
-#Only use major and minor version, ignore build number.
+	#Only use major and minor version, ignore build number.
 	return float(versionSplit[0] + '.' + versionSplit[1])
 #Trys to set a nvidia setting using the nvidia-settings command
 #Checks the stdout for a success message
 def trySetNvidiaSetting(attrLoc, attrName, val):
+	if _emulating: return (True, "Emulating", "Emulating", None)
 	val = str(val)
 	attrArg = '[' + attrLoc + ']/' + attrName + '=' + val
 	result = execCmd(['nvidia-settings', '-a', attrArg])
-	cout = result[1].strip().replace('\n', '').replace(' ', '')
+	cout = result[1].strip().replace(' ', '')
 	display = getXDisplay()
 	check = 'Attribute\'' + attrName + '\'(' + _hostName + display + '[' + attrLoc + '])assignedvalue' + val + '.'
 	return (cout == check, cout, check, result[2])
 #Enables/Disables fan control of the nvidia card
 def trySetFanControlEnabled(enabled, gpu=0):
-	state = '0'
-	if(enabled):
-		state = '1'
+	state = '1' if enabled else '0'
 	return trySetNvidiaSetting('gpu:' + str(gpu), 'GPUFanControlState', state)
 #Sets the fan speed of a nvidia card
 #The legacy variable can be determined by shouldUseLegacyFanSpeed()
 #Read more here https://wiki.archlinux.org/index.php/NVIDIA/Tips_and_tricks#Set_fan_speed_at_login
 def trySetFanSpeed(speed, fan=0, legacy=False):
-	if(speed > 80):
-		speed = 80
-	if(speed < 0):
-		speed = 0
+	if speed < _minRevSpeed: speed = _minRevSpeed
+	if speed > 100 or speed < 0: raise ValueError("Speed must be between 0 and 100%!")
 	attrName = 'GPUTargetFanSpeed'
-	if(legacy):
-		attrName = 'GPUCurrentFanSpeed'
+	if legacy: attrName = 'GPUCurrentFanSpeed'
 	return trySetNvidiaSetting('fan:' + str(fan), attrName, str(int(speed)))
 #Tells you whether or not you should use the legacy method of setting fan speed
 #Read more here https://wiki.archlinux.org/index.php/NVIDIA/Tips_and_tricks#Set_fan_speed_at_login
-def shouldUseLegacyFanSpeed(gpu=0):
-	return getDriverVersion(gpu) <= 349.16
+def shouldUseLegacyFanSpeed(gpu=0): return getDriverVersion(gpu) <= 349.16
 #Fan curve linearly interpolates against a list fo tuples
-def interpFanCurve(fanCurve, temp, incMode):
-	if(incMode and temp < fanCurve[0][0]):
-		return 0
-	elif(not incMode and temp < fanCurve[0][0] - 3):
-		return 0
-	elif(not incMode and temp < fanCurve[0][0]):
-		return -1
-	else:
-		last = fanCurve[0]
-		for i in range(1, len(fanCurve)):
-			cur = fanCurve[i]
-			tMin = last[0]
-			tMax = cur[0]
-			if(temp >= tMin and temp <= tMax):
-				sMin = last[1]
-				sMax = cur[1]
-				m = (sMax - sMin) / (tMax - tMin)
-				b = sMin - m * tMin
-				return m * temp + b
-			last = cur
-	return 80
+def interpFanCurve(fanCurve, temp):
+	last = (0, 0)
+	for i in range(0, len(fanCurve)):
+		cur = fanCurve[i]
+		tMin = last[0]
+		tMax = cur[0]
+		if(temp >= tMin and temp <= tMax):
+			sMin = last[1]
+			sMax = cur[1]
+			m = (sMax - sMin) / (tMax - tMin)
+			b = sMin - m * tMin
+			return m * temp + b
+		last = cur
+	return 50
 
 class FanError(Exception):
 	def __init__(self, msg, reason="Unknown"):
 		self._msg = msg
 		self._reason = reason
-		
+
 	def __str__(self):
 		return self._msg + ": " + self._reason
-class NVFan:
-	def __init__(self, gpu, fan):
-		self._gpu = gpu
-		self._fan = fan
-		self._legacy = shouldUseLegacyFanSpeed()
-		res = self.controllable()
-		if not res[0]:
-			raise FanError("Fan not controllable", res[1])
-	def isLegacy():
-		return self._legacy
-	def controllable(self):
-		res = trySetFanControlEnabled(True)
-		if(not res[0]):
-			return (False, "Unable to enable fan control for gpu %i!" % (self._gpu))
-		res = trySetFanSpeed(30, legacy=self.isLegacy(), fan=self._fan)
-		if(not res[0]):
-			return (False, "Unable to set fan speed for gpu %i, fan %i!" % (self._gpu, self._fan))
-			trySetFanControlEnabled(False)
-		return (True, "OK")
 
 if(__name__ == "__main__"):
-	print("This is a library, not a program!")
+	print_color(NC, "This is a library, not a program!")
